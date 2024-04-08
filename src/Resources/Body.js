5983626f9a8b0c8044eb6256d4fb492c97937536
The-greenpanter <judpenaca@unal.edu.co>
2024-04-07 22:57:39 -0500

    Primeros Modulos

---
+++ src/Resources/Body.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Body = () => {
+  return (
+    <header class="encabezado">
+    <div class="conteiner">
+        <nav class="menu">
+            <a id="btn-acerca-de" class="acercade">Acerca de</a>
+            <a id="btn-Trabajos" class="Diseños">Diseños</a>
+            <a id="btn-contacto" class="contacto">Contacto</a> 
+        </nav>
+        <div class="titlecontainer" id="">
+            <div class="texto">
+                <h1 class="Tituloprincipal">The Green <h1 class="Tituloprincipal" >Panter </h1> </h1>
+                <h2 class="descripcion">Diseñador Digital</h2>
+            </div>
+        </div>
+    </div>
+</header>
+ 
+  )
+}
+
+export default Body
---
+++ src/Resources/footrt.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const footrt = props => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+footrt.propTypes = {
+
+}
+
+export default footrt
