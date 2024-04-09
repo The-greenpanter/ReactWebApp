a0ced134510d3d3adef911053f9cf9d6d071d5bc
The-greenpanter <judpenaca@unal.edu.co>
2024-04-09 10:39:25 -0500

    trabajando en el footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Resources/Header';
 import Body from './Resources/Body';
+import footer from "./Resources/footer";
 import './porta.css'
 
 
 function App() {
   return (
-  <><Header /><Body /></>
+  <><Header /><Body /><footer />
+  </>
   );
 }
 
--- src/Resources/footer.js
+++ src/Resources/footer.js
@@ -1,23 +1,12 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
-const footrt = (props) => {
-    const handleSubmit = (event) => {
-      event.preventDefault(); // Prevent default form submission
-      const formData = new FormData(event.target);
-      const nombre = formData.get('nombre');
-      const correo = formData.get('correo');
-      const mensaje = formData.get('Mensaje');
-  
-      // Do something with the values (e.g., call props.onSubmit or store them in state)
-      props.onSubmit({ nombre, correo, mensaje });
-    };
+const footer = () => {
   return (
   <footer>
     <section class="contacto">
         <div class="container">
             <h3 class="titulo" id="contacto">Contacto</h3>
-            <form class="formulario" onSubmit={handleSubmit}>
+            <form class="formulario">
                 <input class="InsertNombre" type="text" placeholder="Nombre" name="nombre" required></input>
                 <input class="InsertCorreo" type="email" placeholder="Correo" name="correo" required></input>
                 <textarea name="Mensaje" placeholder="Mensaje" required></textarea>
@@ -37,14 +26,4 @@ const footrt = (props) => {
   )
 }
 
-footrt.propTypes = {
-  initialValues: React.PropTypes.shape({
-    nombre: React.PropTypes.string,
-    correo: React.PropTypes.string,
-    Mensaje: React.PropTypes.string,
-  }),
-  onSubmit: React.PropTypes.func.isRequired,
-  errors: React.PropTypes.object,
-};
-
-export default footrt
+export default footer
