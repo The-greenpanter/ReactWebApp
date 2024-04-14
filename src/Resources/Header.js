76f892152a20606acc9f4552aae4ac274be88ed9
The-greenpanter <judpenaca@unal.edu.co>
2024-04-14 17:15:24 -0500

    header with radio

--- src/Resources/Header.js
+++ src/Resources/Header.js
@@ -3,6 +3,9 @@ import React from 'react'
 const Header = () => {
   return (
       <header class="encabezado">
+    <section class="Radio">
+        <iframe src="https://zeno.fm/player/chispero-records" width="100%" height="250" frameborder="0" scrolling="no"></iframe><a href="https://zeno.fm/" target="_blank"  style={{ fontSize: '0.9em', lineHeight: '10px' }}></a>
+    </section>
         <div class="conteiner">
             <nav class="menu">
                 <a id="btn-acerca-de" class="acercade">Acerca de</a>
