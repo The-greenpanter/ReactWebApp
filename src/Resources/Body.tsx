d579332461cca934a17125d993d6c1a51ca4163b
The-greenpanter <judpenaca@unal.edu.co>
2024-05-12 21:40:43 -0500

    Comentando elementos <a> para hacer deploy

--- src/Resources/Body.tsx
+++ src/Resources/Body.tsx
@@ -32,60 +32,60 @@ const Body = () => {
                     </div>
                     <div className={Mainsytle.work}>
                         <div className={Mainsytle.thumb}>
-                            <a href="#" className={Mainsytle.link}>
-                                <a href="#/Articuloflexbox/" className={Mainsytle.link}>
+                            {/* <a href="#" className={Mainsytle.link}> */}
+                                {/* <a href="#/Articuloflexbox/" className={Mainsytle.link}> */}
                                     <img src="img/Landscape.png" alt="" className={Mainsytle.Iuno}></img>
                                     <div className={Mainsytle.descripcion}>
                                         <p className={Mainsytle.smalltittle}>Biologia</p>
                                         <p className={Mainsytle.smalldescription}>Detalles de un Biologo egresado de la UNAL-Sede Bogota </p>
                                     </div>
-                                </a>
-                            </a>   
+                                {/* </a> */}
+                            {/* </a>    */}
                         </div>
                     </div>
                     <div className={Mainsytle.work}>
                         <div className={Mainsytle.thumb}>
-                            <a href="#../src/Practicjquery/index.html" className={Mainsytle.link}>
+                            {/* <a href="#../src/Practicjquery/index.html" className={Mainsytle.link}> */}
                                 <img src="img/Disenho.png" alt="" className={Mainsytle.Iuno}></img>
                                 <div className={Mainsytle.descripcion}>
                                     <p className={Mainsytle.smalltittle}>Diseñado para start up</p>
                                     <p className={Mainsytle.smalldescription}>Diseño web dinamico con jQuery </p>
                                 </div>                    
-                            </a> 
+                            {/* </a>  */}
                         </div>
                     </div>
                     <div className={Mainsytle.work}>     
                         <div className={Mainsytle.thumb}>
-                            <a href="#/TemaParawordpress/index.html" className={Mainsytle.link}>
+                            {/* <a href="#/TemaParawordpress/index.html" className={Mainsytle.link}> */}
                                 <img src="img/PSX_20220321_084354(1).png" alt="" className={Mainsytle.Iuno}></img>
                                 <div className={Mainsytle.descripcion}>
                                     <p className={Mainsytle.smalltittle}>Cafe Euphonia</p>
                                     <p className={Mainsytle.smalldescription}>De la finca a la taza</p>
                                 </div>
-                            </a>   
+                            {/* </a>    */}
                         </div>
                     </div>
                     <div className={Mainsytle.work}>     
                         <div className={Mainsytle.thumb}>
-                            <a href="#" className={Mainsytle.link}>
+                            {/* <a href="#" className={Mainsytle.link}> */}
                                 <img src="img/sun.png" alt="" className={Mainsytle.Iuno}></img>
                                 <div className={Mainsytle.descripcion}>
                                     <p className={Mainsytle.smalltittle}>Title of work</p>
                                     <p className={Mainsytle.smalldescription}>Tools and ideas main</p>
                                 </div>
-                            </a>   
+                            {/* </a>    */}
 
                         </div>
                 </div>
                     <div className={Mainsytle.work}>
                         <div className={Mainsytle.thumb}>
-                            <a href="#/target" className={Mainsytle.link}>
+                            {/* <a href="#/target" className={Mainsytle.link}> */}
                                 <img src="img/Travel.png" alt="" className={Mainsytle.Iuno}></img>
                                 <div className={Mainsytle.descripcion}>
                                     <p className={Mainsytle.smalltittle}>Title of work</p>
                                     <p className={Mainsytle.smalldescription}>Tools and ideas main</p>
                                 </div>
-                            </a>   
+                            {/* </a>    */}
                         </div>
                     </div>
                 </div>     
