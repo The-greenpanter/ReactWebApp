f99c73e4e38e5ccec38eecea3ffcc70f64183977
The-greenpanter <judpenaca@unal.edu.co>
2024-09-05 15:01:59 -0500

    form

--- src/Resources/Footrt.tsx
+++ src/Resources/Footrt.tsx
@@ -3,6 +3,7 @@ import Mainsytle from "../Estilos/output.module.css";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import Finalsection from "./finalSection";
 
 // Validación con Zod
 const schema = z.object({
@@ -52,20 +53,20 @@ const Footrt = () => {
             onSubmit={handleSubmit(onSubmit)}
             method="POST"
             data-netlify="true"
+            data-netlify-recaptcha="true"
           >
             <input type="hidden" name="form-name" value="contact" />
             <input className={Mainsytle.InsertNombre} type="text" placeholder="Name" {...register("name")} />
             {errors.name && <div className={Mainsytle["error-message"]}>{errors.name.message}</div>}
-            
             <input className={Mainsytle.InsertCorreo} type="email" placeholder="Email" {...register("email")} />
             {errors.email && <div className={Mainsytle["error-message"]}>{errors.email.message}</div>}
-            
             <textarea placeholder="Message" {...register("mensaje")} />
             {errors.mensaje && <div className={Mainsytle["error-message"]}>{errors.mensaje.message}</div>}
-            
+            <div data-netlify-recaptcha="true"></div>            
             <input className={Mainsytle.boton} type="submit" value="Send" disabled={isSubmitting} />
           </form>
         </div>
+        <Finalsection/>
       </section>
     </footer>
   );
