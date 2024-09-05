df1d378533a2b8b3c218cc75826a3a28ef11fbf9
The-greenpanter <judpenaca@unal.edu.co>
2024-09-05 10:22:57 -0500

    SocialMediaLinksFix

--- src/Resources/finalSection.tsx
+++ src/Resources/finalSection.tsx
@@ -1,16 +1,18 @@
 import Mainsytle from "../Estilos/output.module.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const finalSection = () => {
   return (
 <section className={Mainsytle["social-media"]}>
     <div className={Mainsytle.container}><a href="https://www.instagram.com/the.green_panter/" className={Mainsytle.IG} aria-label="Instagram">
-  <i className="fa fa-instagram"></i>
+ <FontAwesomeIcon className="fa fa-instagram" icon={faInstagram} />
 </a>
 <a href="https://github.com/The-greenpanter" className={Mainsytle.GitHub} aria-label="GitHub">
-  <i className="fa fa-github"></i>
+  <FontAwesomeIcon className="fa fa-github" icon={faGithub} />
 </a>
 <a href="https://www.linkedin.com/in/juan-diego-peña-castillo-932396138/" className={Mainsytle.Linkin} aria-label="LinkedIn">
-  <i className="fa fa-linkedin"></i>
+   <FontAwesomeIcon className="fa fa-linkedin" icon={faLinkedin} />
 </a>
 
     </div>
